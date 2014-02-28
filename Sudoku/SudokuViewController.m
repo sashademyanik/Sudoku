282f0ea38391645b14ef669982ce3ea7fecd4e89
Sasha Demyanik <alexander.demyanik@gmail.com>
2014-02-28 14:05:54 -0800

    Added menu buttons view

--- Sudoku/SudokuViewController.m
+++ Sudoku/SudokuViewController.m
@@ -8,6 +8,7 @@
 
 #import "SudokuViewController.h"
 #import "SudokuBoard.h"
+#import "MenuButtonView.h"
 
 @interface SudokuViewController ()
 
@@ -28,7 +29,16 @@
     SudokuBoard *sudokuBoardView = [[SudokuBoard alloc] initWithFrame:CGRectMake(origin.x, origin.y, size, size)];
     sudokuBoardView.backgroundColor = [UIColor whiteColor];
     sudokuBoardView.contentMode = UIViewContentModeRedraw;
+    
+    
+    const CGRect sudokuBounds = sudokuBoardView.bounds;
+    MenuButtonView *menuButtonView = [[MenuButtonView alloc] initWithFrame:CGRectMake(sudokuBounds.origin.x, sudokuBounds.origin.y + sudokuBounds.size.height, myBounds.size.width, myBounds.size.height - (sudokuBounds.origin.y + sudokuBounds.size.height) )];
+    menuButtonView.backgroundColor = [UIColor greenColor];
+    
+    
+    
     [self.view addSubview:sudokuBoardView];
+    [self.view addSubview:menuButtonView];
 }
 
 - (void)didReceiveMemoryWarning
